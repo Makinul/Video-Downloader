8b0149cf56859d5291d706a21255587892695580
Makinul Hasan <mcnasim@gmail.com>
2025-06-15 19:15:20 +0600

    feat: Implement download progress and UI enhancements
    Developed-by: Makinul

--- app/src/main/java/com/makinul/instragram/video/downloader/MainViewModel.kt
+++ app/src/main/java/com/makinul/instragram/video/downloader/MainViewModel.kt
@@ -10,24 +10,22 @@ import androidx.lifecycle.viewModelScope
 import com.makinul.instragram.video.downloader.data.model.Post
 import com.makinul.instragram.video.downloader.data.repository.MyRepository
 import com.makinul.instragram.video.downloader.di.httpClient
-import com.makinul.instragram.video.downloader.utils.AppConstant
 import com.makinul.instragram.video.downloader.utils.AppConstant.showLog
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.jvm.javaio.toInputStream
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.File
 import java.io.InputStream
 
 data class MyUiState(
     val message: String = "Initial Message",
     val isLoading: Boolean = false,
+    val progress: Float = -1f,
     val posts: Post? = null,
     val error: String? = null
 )
@@ -39,18 +37,28 @@ class MainViewModel(
     private val _uiState = MutableStateFlow(MyUiState())
     val uiState: StateFlow<MyUiState> = _uiState
 
-    fun fetchInstaVideo() {
+    fun fetchInstaVideo(context: Context, instaUrl: String) {
         viewModelScope.launch {
             _uiState.value = _uiState.value.copy(isLoading = true, error = null)
             try {
                 val post =
-                    myRepository.fetchInstaVideo("https://www.instagram.com/reel/DK1pA-4sC4X/")
+                    myRepository.fetchInstaVideo(instaUrl)
+
                 _uiState.value = _uiState.value.copy(
-                    isLoading = false,
+                    isLoading = true,
+                    progress = 0.01f,
                     posts = post
                 )
 
-                showLog()
+                if (post.video_url.isNullOrEmpty()) {
+                    _uiState.value = _uiState.value.copy(
+                        isLoading = false,
+                        error = "Failed to load instagram video"
+                    )
+                    return@launch
+                }
+
+                downloadInstaVideo(context = context, videoUrl = post.video_url)
             } catch (e: Exception) {
                 print(e.message)
                 _uiState.value = _uiState.value.copy(
@@ -62,10 +70,9 @@ class MainViewModel(
     }
 
 
-    fun downloadInstaVideo(context: Context) {
-        viewModelScope.launch {
-            val videoUrl =
-                "https://scontent-sof1-1.cdninstagram.com/o1/v/t16/f2/m86/AQP59AEN2R6tL5ykE7tpnINARl5jwAJhXXbRsxbypcdMg1VVKJN6lypelTeicGGdsljIe4BtSjlT5BVq2AKG17e-87UhI1rbU6Vz6ig.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNjI4LmJhc2VsaW5lIn0&_nc_cat=105&vs=1019626473639644_1371450734&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC8wMjRCNjQwMjI3Nzc2NzIyQUFBMjBBODdDOEIxQzQ4RV92aWRlb19kYXNoaW5pdC5tcDQVAALIARIAFQIYOnBhc3N0aHJvdWdoX2V2ZXJzdG9yZS9HTHZRR2g1bk5NUUIta29DQU52MUZPel9GNE1yYnFfRUFBQUYVAgLIARIAKAAYABsAFQAAJpy5z6jFv9I%2FFQIoAkMzLBdAQbMzMzMzMxgSZGFzaF9iYXNlbGluZV8xX3YxEQB1%2Fgdl5p0BAA%3D%3D&ccb=9-4&oh=00_AfMgUBHcyhegJklSvIIrVPT7tqKauAEKT_7YmlV2VH6WYQ&oe=685084F3&_nc_sid=d885a2"
+    private suspend fun downloadInstaVideo(context: Context, videoUrl: String) {
+//        val videoUrl =
+//            "https://scontent-sof1-1.cdninstagram.com/o1/v/t16/f2/m86/AQP59AEN2R6tL5ykE7tpnINARl5jwAJhXXbRsxbypcdMg1VVKJN6lypelTeicGGdsljIe4BtSjlT5BVq2AKG17e-87UhI1rbU6Vz6ig.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNjI4LmJhc2VsaW5lIn0&_nc_cat=105&vs=1019626473639644_1371450734&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC8wMjRCNjQwMjI3Nzc2NzIyQUFBMjBBODdDOEIxQzQ4RV92aWRlb19kYXNoaW5pdC5tcDQVAALIARIAFQIYOnBhc3N0aHJvdWdoX2V2ZXJzdG9yZS9HTHZRR2g1bk5NUUIta29DQU52MUZPel9GNE1yYnFfRUFBQUYVAgLIARIAKAAYABsAFQAAJpy5z6jFv9I%2FFQIoAkMzLBdAQbMzMzMzMxgSZGFzaF9iYXNlbGluZV8xX3YxEQB1%2Fgdl5p0BAA%3D%3D&ccb=9-4&oh=00_AfMgUBHcyhegJklSvIIrVPT7tqKauAEKT_7YmlV2VH6WYQ&oe=685084F3&_nc_sid=d885a2"
 //            myRepository.downloadInstaVideo(
 //                instaReelUrl = "https://scontent-sof1-1.cdninstagram.com/o1/v/t16/f2/m86/AQP59AEN2R6tL5ykE7tpnINARl5jwAJhXXbRsxbypcdMg1VVKJN6lypelTeicGGdsljIe4BtSjlT5BVq2AKG17e-87UhI1rbU6Vz6ig.mp4?stp=dst-mp4&efg=eyJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSIsInZlbmNvZGVfdGFnIjoidnRzX3ZvZF91cmxnZW4uY2xpcHMuYzIuNjI4LmJhc2VsaW5lIn0&_nc_cat=105&vs=1019626473639644_1371450734&_nc_vs=HBksFQIYUmlnX3hwdl9yZWVsc19wZXJtYW5lbnRfc3JfcHJvZC8wMjRCNjQwMjI3Nzc2NzIyQUFBMjBBODdDOEIxQzQ4RV92aWRlb19kYXNoaW5pdC5tcDQVAALIARIAFQIYOnBhc3N0aHJvdWdoX2V2ZXJzdG9yZS9HTHZRR2g1bk5NUUIta29DQU52MUZPel9GNE1yYnFfRUFBQUYVAgLIARIAKAAYABsAFQAAJpy5z6jFv9I%2FFQIoAkMzLBdAQbMzMzMzMxgSZGFzaF9iYXNlbGluZV8xX3YxEQB1%2Fgdl5p0BAA%3D%3D&ccb=9-4&oh=00_AfMgUBHcyhegJklSvIIrVPT7tqKauAEKT_7YmlV2VH6WYQ&oe=685084F3&_nc_sid=d885a2",
 //                onProgress = { progress ->
@@ -78,34 +85,40 @@ class MainViewModel(
 //                }
 //            )
 
-            try {
-                // Use the singleton Ktor client to make the request
-                val response = httpClient.get(videoUrl)
+        try {
+            // Use the singleton Ktor client to make the request
+            val response = httpClient.get(videoUrl)
 
-                // Get the body as a ByteReadChannel
-                val channel: ByteReadChannel = response.body()
+            // Get the body as a ByteReadChannel
+            val channel: ByteReadChannel = response.body()
 
-                // Convert the channel to an InputStream and save it
-                val savedUri = saveVideoToGallery(
-                    context = context,
-                    inputStream = channel.toInputStream(), // <-- Ktor provides this extension
-                    fileName = "instagram_video_${System.currentTimeMillis()}.mp4"
+            // Convert the channel to an InputStream and save it
+            val savedUri = saveVideoToGallery(
+                context = context,
+                inputStream = channel.toInputStream(), // <-- Ktor provides this extension
+                fileName = "instagram_video_${System.currentTimeMillis()}.mp4"
+            )
+
+            if (savedUri != null) {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    progress = 1f,
+                    message = "Download complete"
                 )
-
-                if (savedUri != null) {
-                    showLog("savedUri $savedUri")
-                } else {
-                    showLog("FAILURE: Handle error")
-                }
-            } catch (e: Exception) {
-                // Handle Ktor network exceptions (e.g., HttpRequestTimeoutException, ConnectException)
-                e.printStackTrace()
+            } else {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    progress = -1f,
+                    message = "Download failed"
+                )
             }
+        } catch (e: Exception) {
+            // Handle Ktor network exceptions (e.g., HttpRequestTimeoutException, ConnectException)
+            e.printStackTrace()
         }
     }
 
-
-    suspend fun saveVideoToGallery(
+    private suspend fun saveVideoToGallery(
         context: Context,
         inputStream: InputStream,
         fileName: String
--- app/src/main/java/com/makinul/instragram/video/downloader/ui/DownloadScreen.kt
+++ app/src/main/java/com/makinul/instragram/video/downloader/ui/DownloadScreen.kt
@@ -2,7 +2,9 @@ package com.makinul.instragram.video.downloader.ui
 
 import android.Manifest
 import android.os.Build
+import android.widget.ProgressBar
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -19,24 +21,35 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.makinul.instragram.video.downloader.MainViewModel
+import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -45,9 +58,27 @@ fun DownloadScreen(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel = koinViewModel()
 ) {
-    val context = LocalContext.current
 
-    /* Content for Download screen */
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        val permissionState = rememberPermissionState(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        LaunchedEffect(Unit) {
+            if (!permissionState.status.isGranted) {
+                permissionState.launchPermissionRequest()
+            }
+        }
+    }
+
+    val context = LocalContext.current
+    var instaUrl by remember { mutableStateOf("") }
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val currentProgress = remember { mutableFloatStateOf(uiState.progress) }
+
+    val errorMessage = remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize() // Make the box fill the available space
@@ -56,8 +87,11 @@ fun DownloadScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
-            value = "",
-            onValueChange = { },
+            value = instaUrl,
+            onValueChange = { newText ->
+                // 2. Update the state whenever the user types.
+                instaUrl = newText
+            },
             modifier = Modifier.fillMaxWidth(),
             placeholder = { Text("Paste Instagram URL here...") },
             leadingIcon = {
@@ -73,20 +107,114 @@ fun DownloadScreen(
         Spacer(modifier = Modifier.height(8.dp))
 
         Button(
-            onClick = { viewModel.downloadInstaVideo(context = context) },
+            onClick = {
+                if (instaUrl.isEmpty()) {
+                    errorMessage.value = "This field not be empty"
+                    return@Button
+                }
+
+                if (!instaUrl.startsWith("https://www.instagram.com/")) {
+                    errorMessage.value = "Please enter a valid Instagram URL"
+                    return@Button
+                }
+
+                errorMessage.value = ""
+                viewModel.fetchInstaVideo(context = context, instaUrl = instaUrl)
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .height(50.dp),
-            shape = RoundedCornerShape(12.dp)
+            shape = RoundedCornerShape(12.dp),
+            enabled = !uiState.isLoading
         ) {
             Icon(Icons.Filled.Download, contentDescription = null)
             Spacer(modifier = Modifier.width(8.dp))
-            Text("Download Video", fontSize = 16.sp)
+            val buttonText = if (uiState.isLoading) {
+                "Downloading Video"
+            } else {
+                "Download Video"
+            }
+            Text(buttonText, fontSize = 16.sp)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        if (uiState.isLoading) {
+            if (currentProgress.floatValue > 0f)
+                LinearProgressIndicator(
+                    progress = { currentProgress.floatValue },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            else
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth()
+                )
+        } else if (uiState.progress == 1f) {
+            instaUrl = ""
+            errorMessage.value = uiState.message
+        }
+
+        if (errorMessage.value.isNotEmpty()) {
+            Text(errorMessage.value)
         }
 
         Spacer(modifier = Modifier.height(8.dp))
 
         HowToUseCard()
+    }
+}
+
+@Composable
+fun IndeterminateProgressExample() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier.width(150.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IndeterminateProgressPreview() {
+    IndeterminateProgressExample()
+}
+
+@Composable
+fun DeterminateProgressExample() {
+    var currentProgress by remember { mutableFloatStateOf(0f) }
+    var isLoading by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Determinate LinearProgressIndicator: ${(currentProgress * 100).toInt()}%")
+        LinearProgressIndicator(
+            progress = { currentProgress }, // Pass the state here
+            modifier = Modifier.width(150.dp)
+        )
+
+        Spacer(Modifier.height(30.dp))
+
+        Text("Determinate CircularProgressIndicator: ${(currentProgress * 100).toInt()}%")
+        CircularProgressIndicator(
+            progress = { currentProgress }, // Pass the state here
+            modifier = Modifier.width(64.dp)
+        )
+
+        Spacer(Modifier.height(30.dp))
+
+        Button(
+            onClick = { isLoading = true },
+            enabled = !isLoading
+        ) {
+            Text("Start Progress")
+        }
     }
 }
 
